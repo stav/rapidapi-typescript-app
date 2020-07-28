5cdf2f0821115dcbfc4ebd696e1b5c1c43696ce6
Steven Almeroth <sroth77@gmail.com>
2020-07-28 16:17:31 -0400

    external

--- pages/api/prices.ts
+++ pages/api/prices.ts
@@ -1,10 +1,39 @@
 
+async function external(url: string, config?: object): Promise<any> {
+  const res = await fetch(url, config)
+  const data = await res.json()
+  return data
+}
+
 async function get(req, res): Promise<any> {
   return 'Helpful information'
 }
 
 async function post(req, res): Promise<any> {
-  return req.body
+  const params = {
+    denominator: req.body.currency,
+    exchange: req.body.exchange,
+    asset: req.body.symbol,
+  };
+  const priceUrl = new URL('https://crypto-asset-market-data-unified-apis-for-professionals.p.rapidapi.com/api/v1/exchanges/trades');
+  priceUrl.search = (new URLSearchParams(params)).toString();
+
+  const remoteResponse = await external(
+    priceUrl.toString(),
+    {
+      headers: {
+        'x-rapidapi-host': 'crypto-asset-market-data-unified-apis-for-professionals.p.rapidapi.com',
+        'x-rapidapi-key': '[YOUR X-RAPIDAPI-KEY]',
+      }
+    }
+  )
+  const response = Object.assign({
+    currency: req.body.currency,
+    exchange: req.body.exchange,
+    symbol: req.body.symbol,
+  }, remoteResponse)
+
+  return response
 }
 
 export default async function (req, res): Promise<any> {
